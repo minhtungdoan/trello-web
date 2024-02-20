96a0e47b9ea10fe59aeb438ab5fbab4538e97ccf
minhtungdoan <minhtungdoan.work@gmail.com>
2024-02-20 17:46:53 +0700

    Add dark mode toggle button and apply theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
-import { Button } from '@mui/material'
+import { Button, useColorScheme } from '@mui/material'
 import { pink } from '@mui/material/colors'
+
 
 function App() {
 
+  function ModeToggle() {
+    const { mode, setMode } = useColorScheme()
+    return (
+      <Button
+        onClick={() => {
+          setMode(mode === 'light' ? 'dark' : 'light')
+        }}
+      >
+        {mode === 'light' ? 'Turn dark' : 'Turn light'}
+      </Button>
+    )
+  }
+
   return (
     <>
+      <ModeToggle />
       <div>12345</div>
       <div>Minhtungdoan</div>
       <Button variant="text">Text</Button>
