5f058ba9ec3a6ab856229ff4d0394b33f5367ec8
minhtungdoan <minhtungdoan.work@gmail.com>
2024-03-20 20:37:17 +0700

    process env build mode

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 // import svgr from 'vite-plugin-svgr'
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Cho phep Vite su dung process.env, mac dinh la import.meta.env
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     react(),
-    svgr(),
+    svgr()
   ],
   // base: './'
   resolve: {
